67c23b61f358757f59a23eae95e81e4a179c927a
akshaya <akshayagoud101@gmail.com>
2021-09-28 21:25:54 +0530

    add static and methods

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
+// Importing Env Variables
+require("dotenv").config();
+
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 
+// microservice routes
+import Auth from "./API/Auth";
+
+// Database connection
+import ConnectDB from "./database/connection";
+
 const zomato = express();
+
 
 // application middlewares
 zomato.use(express.json());
@@ -10,6 +20,15 @@ zomato.use(express.urlencoded({ extended: false }));
 zomato.use(helmet());
 zomato.use(cors());
 
+// Application Routes
+zomato.use("/auth", Auth);
+
 zomato.get("/", (req,res) => res.json({ message: "Setup success"}));
 
-zomato.listen(4000, () => console.log("Server is running"));
+
+zomato.listen(4000, () =>
+  ConnectDB().then(() => console.log("Server is running 🚀"))
+    .catch(() =>
+      console.log("Server is running, but database connection failed... ")
+    )
+);
