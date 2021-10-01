813fad9c75fc352ee21c4fa36bfa29f8588a77cc
akshaya <akshayagoud101@gmail.com>
2021-10-01 22:25:11 +0530

    add google auth

--- server/API/Auth/index.js
+++ server/API/Auth/index.js
@@ -2,6 +2,8 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import passport from "passport";
+
 
 const Router = express.Router();
 
@@ -47,7 +49,38 @@ Router.post("/signin", async (req, res) => {
   }
 });
 
+/*
+Route     /google
+Des       Google Signin
+Params    none
+Access    Public
+Method    GET  
+*/
+Router.get(
+  "/google",
+  passport.authenticate("google", {
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
+    ],
+  })
+);
 
+/*
+Route     /google/callback
+Des       Google Signin Callback
+Params    none
+Access    Public
+Method    GET  
+*/
+Router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    return res.json({token:req.session.passport.user.token});
+  
+  }
+);
 
 
 export default Router;
--- server/index.js
+++ server/index.js
@@ -4,6 +4,11 @@ require("dotenv").config();
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import passport from "passport";
+
+// configs
+import googleAuthConfig from "./config/google.config";
+
 
 // microservice routes
 import Auth from "./API/Auth";
@@ -19,6 +24,13 @@ zomato.use(express.json());
 zomato.use(express.urlencoded({ extended: false }));
 zomato.use(helmet());
 zomato.use(cors());
+zomato.use(passport.initialize());
+
+
+
+// passport cofiguration
+googleAuthConfig(passport);
+
 
 // Application Routes
 zomato.use("/auth", Auth);
